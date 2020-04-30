3a664b0ed742e28eca08efec48f1b7eb962b1d81
rexfordnana <rexfordnana@gmail.com>
2020-04-29 21:19:07 -0400

    simplified server.js, moved routes to central location

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const rsvps = require('../models/rsvp.js');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('this is the home page');
+  res.send('A beautiful website coming soon');
+});
+
+router.get('/rsvp', (req, res) => {
+  res.send(rsvps);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,11 @@
 const express = require('express');
 const homeRouter = require('./routes/index.js');
-const rsvps = require('./models/rsvp.js');
 
-/*
- * instantiate express
- */
-
+//instantiate express
 const server = new express();
-console.log(rsvps);
-//create Routes
-// server.get("/user", (req, res) => {
-//   res.send({ name: "rex" });
-// });
-server.get('/other', (req, res) => {
-  res.send('this is the other page');
-});
-server.get('/rsvp', (req, res) => {
-  res.send('this is the posts page');
-  res.send(posts);
-});
 
 //use a middleware
-server.use('/home', homeRouter);
+server.use('/', homeRouter);
 
 //start the server
 server.listen(3000, 'localhost', () => {
