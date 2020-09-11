a3f30bece153f77f90205cfafe0da1a61cfe2a17
Rexford <rexford_aboagye@comcast.com>
2020-09-10 20:09:18 -0400

    working smtp mail service

--- server.js
+++ server.js
@@ -1,50 +1,40 @@
 import express from 'express';
 import { childLogger } from './lib/logger.js';
 import bodyParser from 'body-parser';
-import expressValidator from 'express-validator';
 import DynamoDb from './lib/dynamo.js';
+import SendMail from './lib/send-mail.js';
 import { Rsvp } from './models/rsvp.js';
 
-const { body, validationResult } = expressValidator;
 const app = express();
 const port = process.env.PORT || 8081;
 const log = childLogger({ op: 'In index js' });
 const dynamoDb = new DynamoDb();
 
+//Define some middlewares
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Define routes
 app.get('/', (req, res) => {
   res.render('index.html');
 });
 
-app.post(
-  '/rsvp',
-  [
-    body('name').isLength({ min: 1 }),
-    // body('email').isEmail,
-    //   body('attending').isString,
-    // body('message').isString,
-  ],
-  (req, res) => {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    log.info('In rsvp ...');
-    console.log(req.body)
-    const errors = validationResult(req);
-    const rsvpObject = new Rsvp(req.body);
-    // console.log(rsvpObject.formatForDynamo());
-    if (!errors.isEmpty()) {
-      log.error(errors);
-      res.redirect('/');
-    } else {
-      dynamoDb.writeToDynamo(rsvpObject.formatForDynamo());
-    }
-    // log.info(req.body);
-    res.redirect('/');
-  }
-);
+app.post('/rsvp', (req, res) => {
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  log.info('In rsvp ...');
+  const rsvpObject = new Rsvp(req.body);
+  // console.log(rsvpObject.formatForDynamo());
+  console.log('do nothing');
+  // dynamoDb.writeToDynamo(rsvpObject.formatForDynamo());
+
+  //send email after saving to dynamo
+  const mailer = new SendMail(rsvpObject.email);
+  mailer.send();
+  // log.info(req.body);
+  res.redirect('/');
+});
 
 app.listen(port, () => {
   log.info({ port: port }, 'App started and listening ...');
