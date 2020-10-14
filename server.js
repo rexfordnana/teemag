0d8fce51d9958abc445b2e4cfed710b8d2163445
Rexford <rexford_aboagye@comcast.com>
2020-10-13 22:09:44 -0400

    add new routes to  server

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import session from 'express-session'
+import config from 'config'
+import moment from 'moment'
 import { childLogger } from './lib/logger.js';
 import bodyParser from 'body-parser';
 import DynamoDb from './lib/dynamo.js';
@@ -15,11 +18,19 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
 // Define routes
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/admin', (req, res) => {
+  res.render('sign-in')
+})
 app.post('/rsvp', (req, res) => {
   log.info({op: 'rsvp-post'}, 'Inside rsvp post handler');
 
@@ -41,7 +52,41 @@ app.post('/rsvp', (req, res) => {
     }
   });
 })
-
+app.post('/auth', (req, res) => {
+  log.info({op: 'auth'}, 'Inside auth handler');
+	const username = req.body.username;
+  const password = req.body.password;
+  let loggedInUser = [];
+	if (username && password) {
+    loggedInUser = config.get("accounts").filter((account)=> account.username == username && account.password == password)
+    log.info('Getting user information')
+    if (loggedInUser.length > 0) {
+      req.session.loggedin = true;
+      req.session.username = username;
+      res.redirect('/admin-page');
+    } else {
+      res.render('sign-in', {message:'Incorrect Username and/or Password!'});
+    }			
+    res.end();
+	} else {
+		res.render('sign-in', {message:'Please enter Username and Password!'});
+		res.end();
+	}
+});
+app.get('/admin-page', (req, res) => {
+	if (req.session.loggedin) {
+    dynamoDb.getAllData((data, err) => {
+      if (data) {
+        console.log('Inside render')
+        res.render('admin-page', {rsvpObject: data.Items.sort((a,b) => b.created_on - a.created_on), moment: moment } );
+      }
+      else log.error('Failed to get all data', err)
+    })
+	} else {
+		res.send('Please login to view this page!');
+	}
+	// res.end();
+});
 app.listen(port, () => {
   log.info({ port: port }, 'App started and listening ...');
 });
