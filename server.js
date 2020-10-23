77185d857d93ac096c15a003daa1a12620b7eb38
Rexford <rexford_aboagye@comcast.com>
2020-10-23 09:18:42 -0400

    add captcha to  rsvp form

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import session from 'express-session'
 import config from 'config'
 import moment from 'moment'
+import axios from 'axios'
 import { childLogger } from './lib/logger.js';
 import bodyParser from 'body-parser';
 import DynamoDb from './lib/dynamo.js';
@@ -26,32 +27,56 @@ app.use(session({
 
 // Define routes
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index',{ errors: {}});
 });
 app.get('/admin', (req, res) => {
   res.render('sign-in')
 })
-app.post('/rsvp', (req, res) => {
+
+app.post('/rsvp', async (req, res) => {
   log.info({op: 'rsvp-post'}, 'Inside rsvp post handler');
 
   //format request body into savable rsvp object
   const rsvpObject = new Rsvp(req.body);
-  const email = rsvpObject.email;
+  const email = rsvpObject.email.toLowerCase();
+  const captcha = req.body['g-recaptcha-response'];
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const captchaSecret = config.get('captchaSecret')
+  console.log('Captcha secret', captchaSecret)
 
-  //first check if email already registered
-  dynamoDb.readFromDynamo(email, (dynamoResponse)=>{
-    if( dynamoResponse.Count > 0 ) {
-      log.info(email, ' This email has already registered!')
-      res.render('index', { errors: 'Your email has already registered'})
-    }else{
-      dynamoDb.writeToDynamo(rsvpObject.formatForDynamo());
-      //send email after saving to dynamo
-      // const mailer = new SendMail(rsvpObject.email);
-      // mailer.send();
-      res.render('index', {alert: true, alertTitle: 'Thank you! We are honored', alertMessage: 'You’ll recieve a confirmation email!' });
+  log.info('capture from form ', captcha)
+
+  // g-recaptcha-response is the key that browser will generate upon form submit.
+  // if its blank or null means user has not selected the captcha, so return the error.
+  if (captcha === undefined || captcha === '' || captcha === null) {
+    return res.render('index', {errors: { captcha: 'Click captcha'}});
+  }
+  try{
+    const captchaVerified = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${captchaSecret}&response=${captcha}&remoteip=${ip}`)
+    console.log('------------>', captchaVerified)
+    if (captchaVerified.data.success){
+      //first check if email already registered
+      dynamoDb.readFromDynamo(email, (dynamoResponse)=>{
+        if( dynamoResponse.Count > 0 ) {
+          log.info(email, 'This email has already registered!')
+          res.render('index', { errors: { email: 'Your email has already registered'}})
+        } else {
+          dynamoDb.writeToDynamo(rsvpObject.formatForDynamo());
+          //send email after saving to dynamo
+          // const mailer = new SendMail(rsvpObject.email);
+          // mailer.send();
+          res.render('index', {alert: true, alertTitle: 'Thank you! We are honored', alertMessage: 'You’ll recieve a confirmation email!' });
+        }
+      });
+    } else {
+      log.error('verifcation failed', captchaVerified.data['error-codes']);
+      res.end()
     }
-  });
+  } catch(error) {
+    log.error({op:  'rsvp-captcha', error}, 'Failed to verify captcha')
+  }
 })
+
 app.post('/auth', (req, res) => {
   log.info({op: 'auth'}, 'Inside auth handler');
 	const username = req.body.username;
@@ -77,7 +102,6 @@ app.get('/admin-page', (req, res) => {
 	if (req.session.loggedin) {
     dynamoDb.getAllData((data, err) => {
       if (data) {
-        console.log('Inside render')
         res.render('admin-page', {rsvpObject: data.Items.sort((a,b) => b.created_on - a.created_on), moment: moment } );
       }
       else log.error('Failed to get all data', err)
